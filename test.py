c17c9df50789d8616290451cfc9751438d1cc88a
everinlove <62162606+everinlove@users.noreply.github.com>
2024-08-04 20:24:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import requests
 
 # 目标URL
-url = 'https://www.baidu.com/'
+url = 'http://www.baidu.com/'
 
 # 发送GET请求
 response = requests.get(url)
 
 # 检查请求是否成功
 if response.status_code == 200:
-    # 将网页内容写入到res.txt文件中
-    with open('res.txt', 'w', encoding='utf-8') as file:
+    # 将网页内容写入到test.txt文件中
+    with open('test.txt', 'w', encoding='utf-8') as file:
         file.write(response.text)
 else:
     print(f"请求失败，状态码：{response.status_code}")
