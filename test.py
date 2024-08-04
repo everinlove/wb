45bdeb6f920567bb1824589234842a2e6ff3040a
everinlove <62162606+everinlove@users.noreply.github.com>
2024-08-04 20:29:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ response = requests.get(url)
 # 检查请求是否成功
 if response.status_code == 200:
     # 将网页内容写入到test.txt文件中
+    print(response.text)
     with open('test.txt', 'w', encoding='utf-8') as file:
         file.write(response.text)
 else:
